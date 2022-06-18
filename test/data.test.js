24d4fc1ab300361e6b5d7494b7bc741df5be74b9
dathoang <dathoang997@gmail.com>
2022-06-18 16:53:47 +0700

    show error location in directory

--- test/data.test.js
+++ test/data.test.js
@@ -1,21 +1,22 @@
-var path = require('path');
-var fs = require('fs');
-const searchFile = './';
-const specialNetwork = ['binancesmart','ethereumclassic', 'TC-TEST', 'bitcoins', 'BTC-TEST']
+const path = require('path')
+const fs = require('fs')
+const jsonlint = require("jsonlint")
+const searchFile = './'
+const specialNetwork = ['binancesmart', 'ethereumclassic', 'TC-TEST', 'bitcoins', 'BTC-TEST']
 
 const isValidJson = (string) => {
     try {
-      JSON.parse(string)
-      return true
+        jsonlint.parse(string)
+        return true
     } catch (error) {
         console.log(error)
-      return false
+        return false
     }
-  }
+}
 
-  const getAllFiles = (dirPath, arrayOfFiles) => {
-      files = fs.readdirSync(dirPath)
-      arrayOfFiles = arrayOfFiles || []
+const getAllFiles = (dirPath, arrayOfFiles) => {
+    files = fs.readdirSync(dirPath)
+    arrayOfFiles = arrayOfFiles || []
     files.forEach(function (file) {
         if (file.search('node_module') == -1 && file.search('.git') == -1 && file.search('yarn') == -1) {
             if (fs.statSync(dirPath + "/" + file).isDirectory()) {
@@ -32,7 +33,7 @@ const getNetworks = () => {
     let networkArr = []
     let networkContent = fs.readFileSync('./wallet/tokens/get_public_network', 'utf8')
     let networkObj = JSON.parse(networkContent)
-    for(let i = 0; i < networkObj.data.length; i++) {
+    for (let i = 0; i < networkObj.data.length; i++) {
         networkArr.push(networkObj.data[i].networks)
     }
     let result = networkArr.concat(specialNetwork)
@@ -43,8 +44,8 @@ const isValidMobile = () => {
     let networkArr = getNetworks()
     let mobileContent = fs.readFileSync('./wallet/tokens/mobile', 'utf8')
     let mobileObj = JSON.parse(mobileContent)
-    for(let i = 0; i < mobileObj.length; i++){
-        if(networkArr.indexOf(mobileObj[i].network) != -1){
+    for (let i = 0; i < mobileObj.length; i++) {
+        if (networkArr.indexOf(mobileObj[i].network) != -1) {
             expect(true).toEqual(true)
         } else {
             expect(false).toEqual(true)
@@ -56,8 +57,8 @@ const isValidMobilev2 = () => {
     let networkArr = getNetworks()
     let mobileContent = fs.readFileSync('./wallet/tokens/mobilev2', 'utf8')
     let mobileObj = JSON.parse(mobileContent)
-    for(let i = 0; i < mobileObj.length; i++){
-        if(networkArr.indexOf(mobileObj[i].network) != -1){
+    for (let i = 0; i < mobileObj.length; i++) {
+        if (networkArr.indexOf(mobileObj[i].network) != -1) {
             expect(true).toEqual(true)
         } else {
             expect(false).toEqual(true)
@@ -69,8 +70,8 @@ const isValidToken = () => {
     let networkArr = getNetworks()
     let mobileContent = fs.readFileSync('./wallet/lists/token', 'utf8')
     let mobileObj = JSON.parse(mobileContent)
-    for(let i = 0; i < mobileObj.length; i++){
-        if(networkArr.indexOf(mobileObj[i].network) != -1){
+    for (let i = 0; i < mobileObj.length; i++) {
+        if (networkArr.indexOf(mobileObj[i].network) != -1) {
             expect(true).toEqual(true)
         } else {
             expect(false).toEqual(true)
@@ -82,8 +83,8 @@ const isValidListToken = () => {
     let networkArr = getNetworks()
     let mobileContent = fs.readFileSync('./wallet/list-token', 'utf8')
     let mobileObj = JSON.parse(mobileContent)
-    for(let i = 0; i < mobileObj.length; i++){
-        if(networkArr.indexOf(mobileObj[i].network) != -1){
+    for (let i = 0; i < mobileObj.length; i++) {
+        if (networkArr.indexOf(mobileObj[i].network) != -1) {
             expect(true).toEqual(true)
         } else {
             expect(false).toEqual(true)
@@ -94,19 +95,19 @@ const isValidListToken = () => {
 it('validate JSON file', async () => {
     let files = await getAllFiles(searchFile)
     const contents = await Promise.all(files.map((file) => {
-        if((file.search('wallet/') != -1)){
+        if ((file.search('wallet/') != -1)) {
             let result = [fs.readFileSync('./' + file, 'utf8'), file]
             return result
-      } else if((file.search('.json') != -1)){
-        let result = [fs.readFileSync('./' + file, 'utf8'), file]
+        } else if ((file.search('.json') != -1)) {
+            let result = [fs.readFileSync('./' + file, 'utf8'), file]
             return result
-    }
+        }
         return null
     }))
 
     contents.forEach((content) => {
-        if(content != null){
-            if(isValidJson(content[0]) == true){
+        if (content != null) {
+            if (isValidJson(content[0]) == true) {
                 expect(true).toEqual(true)
             } else {
                 console.log('!!!! False at ', content[1])
@@ -116,18 +117,18 @@ it('validate JSON file', async () => {
     })
 })
 
-// test("test validation mobile", () => {
-//     isValidMobile()
-// })
+test("test validation mobile", () => {
+    isValidMobile()
+})
 
-// test("test validation mobilevs", () => {
-//     isValidMobilev2()
-// })
+test("test validation mobilevs", () => {
+    isValidMobilev2()
+})
 
-// test("test validation list-token", () => {
-//     isValidToken()
-// })
+test("test validation list-token", () => {
+    isValidToken()
+})
 
-// test("test validation token", () => {
-//     isValidListToken()
-// })
+test("test validation token", () => {
+    isValidListToken()
+})
